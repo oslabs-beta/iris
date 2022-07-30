f9ba2441ff92a21b18c169a6e0f20f48fa76d788
Brennan <blee3395@gmail.com>
2022-07-30 11:10:12 -0400

    Refactored demo with KafkaJS and incorporated integer type schema for Avro

--- consumer/index.js
+++ consumer/index.js
@@ -1,19 +1,69 @@
 console.log('consumer running')
 
-import Kafka from 'node-rdkafka';
-import eventType from '../eventType.js';
+import { Kafka } from "kafkajs"
+import eventInteger from '../eventTypes/eventInteger.js';
 
-const consumer = Kafka.KafkaConsumer({
-  'group.id':'kafka101',
-  'metadata.broker.list': 'localhost:9092'
-}, {});
 
-consumer.connect();
-
-consumer.on('ready', () => {
-  console.log('consumer ready...')
-  consumer.subscribe(['test']);
-  consumer.consume();
-}).on('data', (data) => {
-  console.log('received message: ', eventType.fromBuffer(data.value))
+const kafka = new Kafka({
+  clientId:'iris',
+  brokers:['localhost:9092']
 })
+
+const consumer = kafka.consumer({groupId:'iris'})
+
+await consumer.connect()
+
+await consumer.subscribe({
+  topics:['test1', 'test2'],
+  fromBeginning: true
+})
+
+await consumer.run({
+  eachMessage: async({topic, partition, message}) => {
+    if (topic === 'test1') {
+      console.log('received message: ', eventInteger.fromBuffer(message.value))
+    }
+    else if (topic === 'test2') {
+      console.log('receieved message: ', {
+        value: message.value.toString(),
+      })
+    }
+  }
+})
+
+/**
+ * Implementation with node-rdkafka npm module
+ */
+// import Kafka from 'node-rdkafka';
+// import eventType from '../eventType.js';
+
+// const consumer1 = Kafka.KafkaConsumer({
+//   'group.id':'kafka101',
+//   'metadata.broker.list': 'localhost:9092'
+// }, {});
+
+// consumer1.connect();
+
+// consumer1.on('ready', () => {
+//   console.log('consumer1 ready...')
+//   consumer1.subscribe(['test1']);
+//   consumer1.consume();
+// }).on('data', (data) => {
+//   console.log('received message: ', eventType.fromBuffer(data.value))
+// })
+
+
+// const consumer2 = Kafka.KafkaConsumer({
+//   'group.id':'kafka101',
+//   'metadata.broker.list': 'localhost:9092'
+// }, {});
+
+// consumer2.connect();
+
+// consumer2.on('ready', () => {
+//   console.log('consumer2 ready...')
+//   consumer2.subscribe(['test2']);
+//   consumer2.consume();
+// }).on('data', (data) => {
+//   console.log('received message: ', eventType.fromBuffer(data.value))
+// })
