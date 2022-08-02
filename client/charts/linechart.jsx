538ff288b676f992ad8a5f99f4b6bb3c86cea606
Michael Reyes <Michaelreyes499@gmail.com>
2022-08-01 21:11:52 -0400

    charts work

--- client/charts/linechart.jsx
+++ client/charts/linechart.jsx
@@ -1,58 +1,75 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import Chart from 'chart.js/auto';
+// import axios from 'axios';
 import mock1h from './dummyData/mockData_1h';
 import mock6h from './dummyData/mockData_6h';
 
 // no need for axios request, will be passed as props later
 
-function LineGraph(props) {
-    // need to update for labels (time) and data (metrics) when props is received
+function LineChart(props) {
     const [lineChartMetric, updateLine] = useState({
-        labels: ["filler"], // change for axis
-        datasets: [
-            {
-                labels: "filler",
-                data: ["0"],
-                borderColor: "gray"
-            }
-        ]
-    });
-
-    // manipulate our 1 hour data for now
-    // we need to convert our unix timestamps to regular time stamps
-    // need to create an array to house our metrics, correlated to specific time stamp
-    const valueArray = mock1h.data.result[0].values;
-    console.log(valueArray.length);
-
-    // parse through valueArray and house our timestampArr and metricsArr
-    const timestampArr = [];
-    const metricsArr = [];
-
-    for (let i = 0; i < valueArray.length; i++) {
-        timestampArr.push(
-            new Date(Number(valueArray[i][0]) * 1000).toLocaleString() // mult by 1000 given valueArray comes in as seconds
-        );
-        metricsArr.push(Number(valueArray[i][1]));
-    }
-
-    // set our state - updateLine with our new time stamps and metrics data
-    updateLine({
         // labels will be for date -> most likely going to do a cache arr as date.time
-        labels: timestampArr,
+        labels: ['filler'],
         datasets: [
             {
-                label: "Price",
-                data: metricsArr,
+                label: "filler",
+                data: [0],
                 borderColor: "gray"
             }
         ]
     });
 
+    useEffect(() => {
+        // manipulate our 1 hour data for now
+        // we need to convert our unix timestamps to regular time stamps
+        // need to create an array to house our metrics, correlated to specific time stamp
+        const results = mock1h.data.result; // results here is an array
+        const topicData = {};
+
+        // Assign information in topicData to have a header of 'topic' (key)
+        // Assign array of values to topic data (value)
+        for (let j = 0; j < results.length; j++) {
+            if (results[j].metric.topic) {
+                topicData[results[j].metric.topic] = results[j].values;
+            }
+        }
+
+        // need to update for labels (time) and data (metric)
+
+        // parse through valueArray and house our timestampArr and metricsArr
+        const threeLineCharts = []
+        const timestampArr = [];
+        let metricsArr = [];
+        // iterating through out object
+        for (let [topic, value] of Object.entries(topicData)) {
+            let timeArrBuilt = false;
+            for (let i = 0; i < value.length; i++) { // { topic: [timestamp , another time] }
+                if (!timeArrBuilt) {
+                    timestampArr.push(
+                        new Date(Number(value[i][0]) * 1000).toLocaleString() // mult by 1000 given value comes in as seconds
+                    );
+                }
+                metricsArr.push(Number(value[i][1]));
+            }
+            threeLineCharts.push({
+                label: topic,
+                data: metricsArr
+            });
+            metricsArr = [];
+        }
+
+        // set our state - updateLine with our new time stamps and metrics data
+        updateLine({
+            // labels will be for date -> most likely going to do a cache arr as date.time
+            labels: timestampArr,
+            datasets: threeLineCharts // this is an array of 3 subarrays -> subarray [line data]
+        });
+    }, [])
     return <Line id="graph" data={lineChartMetric} />;
 }
 
-module.exports = LineGraph;
+export default LineChart;
 
 
 /*
---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LineChart from '../charts/LineChart.jsx'
+
+
+const App = (props) => {
+
+  return (
+    <>
+      <div><h2>Hi</h2></div>
+      <LineChart />
+    </>
+
+  )
+
+}
+
+export default App;
