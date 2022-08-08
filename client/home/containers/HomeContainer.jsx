4d5d04bd5e4bc443ac23e7ca3663317b756685b8
Michael Reyes <Michaelreyes499@gmail.com>
2022-08-08 15:23:21 -0400

    fixed merges

--- client/home/containers/HomeContainer.jsx
+++ client/home/containers/HomeContainer.jsx
@@ -4,50 +4,10 @@ import LeftPane from './LeftPane.jsx';
 
 
 function HomeContainer(props) {
-<<<<<<< HEAD
     return (
         <div id='homePage'>
             <LeftPane />
             <RightPane />
-=======
-    // implement adding new graphs logic 
-    // always want one graph
-    const [currentGraphs, setGraphs] = useState([]);
-
-    // function that will create new graphs onClick button id = newGraph
-    function createGraph(e) {
-        // const type = e.target.id.slice(3);
-        // console.log('type', type)
-        const holder = [...currentGraphs];
-        console.log('holder, ', String(currentGraphs.length + 1))
-        holder.push(<GraphContainer chartID={String(currentGraphs.length + 1)} key = {`graphContainer${currentGraphs.length + 1}`}/>);
-        setGraphs(holder)
-
-        const reqBody = {
-            metric: 'kafka_server_broker_topic_metrics_bytesinpersec_rate',
-            timeFrame: '5m',
-            chartID: String(currentGraphs.length + 1)
-        }
-
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(reqBody)
-        })
-            .then((formattedData) => {
-                console.log(formattedData)
-            })
-            .catch(err => {
-                console.log('Error thrown in POST request in graphContainer: ', err)
-            })
-    };
-
-    return (
-        <div className='homePage'>
-            <button id='newGraphContainer' onClick={(e) => createGraph(e)}>Add a Graph</button>
-            {/* <button id='newBar' onClick={(e) => createGraph(e)}>Add a Histogram</button> */}
-            <div id='graphFeed'>{currentGraphs}</div>
->>>>>>> 7518d7cf773dec310c315a92beb2a0114ce3fa33
         </div>
     )
 };
