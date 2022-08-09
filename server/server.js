2c63e63e78f7555e6f5462e3f07f083c9bf379e5
JohnLeGitHub <102982879+JohnLeGit@users.noreply.github.com>
2022-08-08 20:34:48 -0400

    fixing and cleaning up the code

--- server/server.js
+++ server/server.js
@@ -249,23 +249,3 @@ module.exports = app;
 
 
 
-
-// const register = new client.Registry();
-// client.collectDefaultMetrics({
-//     app: 'iris',
-//     prefix: 'kafka_controller_',
-//     timeout: 10000,
-//     gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
-//     register
-// });
-
-// app.get('/metrics/', async (req, res) => {
-//     res.setHeader('Content-Type', register.contentType);
-//     // const data = await register.getSingleMetric('node_process_cpu_user_seconds_total');
-//     const data = await register.metrics()
-//     console.log('register metrics: ', data)
-//     // console.log('inside prom client serverjs', data, typeof data)
-//     res.send(data)
-// });
-
-// app.listen(8080, () => console.log('Server is running on http://localhost:8080, metrics are exposed on http://localhost:8080/metrics'));
