912c9c2e85e9481b8689f89c09e59c6db627dbef
Brennan <blee3395@gmail.com>
2022-08-10 15:49:51 -0400

    Fixed styling and updated navBar

--- client/home/components/NavBar.jsx
+++ client/home/components/NavBar.jsx
@@ -3,7 +3,43 @@ import { Link } from 'react-router-dom';
 import irisLogo from '../assets/IRIS_logo_v1.png'
 import irisName from '../assets/IRIS_name_v1.png'
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Our MVP', 'Our Prices', 'Our Story'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 function NavBar() {
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
         <nav className='navbar'>
             <div className="logo">
@@ -11,28 +47,55 @@ function NavBar() {
                 <a href='/' className='site-title'><img src={irisName} id = "iris-logo"/></a>
             </div>
 
-            <ul>
-                <li>
-                    <a href='/home'>Metrics</a>
-                </li>
-                <li>
-                    <a href='/login'>Login</a>
-                </li>
-                <li>
-                    <a href='/signup'>Signup</a>
-                </li>
-            </ul>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ marginLeft: 'auto', flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
         </nav>
     )
 }
-
-// return (
-// <div>
-//     {/* <Link to={{
-//           pathname: ,
-//           state: 
-//           }}></Link> */}
-// </div>
-// )
 
 export default NavBar;
