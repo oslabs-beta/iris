b2a75ef87d724ed43c440118895d96d766086784
Walter Li <106818226+findwalle@users.noreply.github.com>
2022-08-13 12:18:16 -0400

    added historical with michael

--- client/home/containers/HistoricalGraphContainer.jsx
+++ client/home/containers/HistoricalGraphContainer.jsx
@@ -1,84 +1,14 @@
 import 'flatpickr/dist/themes/airbnb.css' // removes flatpicker backround
 import React, { Component, useState, useEffect } from 'react';
 import LineChart from '../components/charts/LineChart.jsx';
-import io from 'socket.io-client';
 import mock1h from '../dummyData/mockData_1h';
-import e from 'cors';
 import Flatpickr from "react-flatpickr"
 
-
-const socket = io();
-
-function LineGraphContainer(props) {
-  // Exposed props.chartID when creating chart at Page Level Component
-  // Use dummy value of '1' for unit testing
+function HistoricalGraphContainer(props) {
   const { chartID } = props;
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
   const [chartData, setChartData] = useState(mock1h.data.result);
-
-  socket.on('connect', () => {
-    console.log('socket connected')
-  });
-
-  socket.on(chartID, (data) => {
-    setChartData(data)
-  });
-
-  socket.on('connect_error', (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
-
-
-  async function handleDynamicMetrics(e) {
-    // grab metric by pulling value from our select id
-    const metrics = e.target.parentNode.querySelector('#metric').value;
-    const timeFrame = e.target.parentNode.querySelector('#timeframe').value;
-
-    let reqBody;
-
-    // FIX CHART ID 
-    if (metrics === '' && timeFrame === '') {
-      alert("Must Choose a Metric and Timeframe")
-      return;
-    }
-    if (metrics !== '' && timeFrame === '') {
-      reqBody = {
-        metric: metrics,
-        timeFrame: '5m',
-        chartID: chartID
-      }
-    }
-    else if (metrics === '' && timeFrame !== '') {
-      reqBody = {
-        metric: 'kafka_server_replica_fetcher_manager_maxlag_value',
-        timeFrame: timeFrame,
-        chartID: chartID
-      }
-    }
-    else {
-      reqBody = {
-        metric: metrics,
-        timeFrame: timeFrame,
-        chartID: chartID
-      }
-    }
-
-    console.log('reqBody: ', reqBody)
-
-    await fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(reqBody)
-    })
-      .then((formattedData) => {
-        console.log(formattedData)
-      })
-      .catch(err => {
-        console.log('Error thrown in POST request in graphContainer: ', err)
-      })
-
-    // breakout of function
-    return;
-  }
 
   function handleChartDelete(e) {
     // need to target the chart with chartID 
@@ -102,11 +32,39 @@ function LineGraphContainer(props) {
       .catch(err => {
         console.log('Error thrown in POST request  in graphContainer: ', err)
       })
-    // no return;
   }
 
-  // return our render
-  // need 2 drop downs -> 1) metrics, 2) timeframe
+  async function handleHistorical(e) {
+    console.log('starting historical metric fetch')
+    
+    const metric = e.target.parentNode.querySelector('#metric').value
+    const startTime = (new Date(e.target.parentNode.querySelector('#startTime').value)).getTime() / 1000;
+    const endTime = (new Date(e.target.parentNode.querySelector('#endTime').value)).getTime() / 1000;
+
+    console.log('metric: ', metric)
+    console.log('startTime: ', startTime)
+    console.log('endTime: ', endTime)
+
+    // await fetch('/historical', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(
+    //     { 
+    //       metric: metric
+    //       startTime: startTime,
+    //       endTime: endTime 
+    //     }
+    //   )
+    // })
+    //   .then(res => res.json())
+    //   .then((data) => {
+    //     console.log('fetch post success')
+    //   })
+    //   .catch(err => {
+    //     console.log('Error thrown in POST request  in graphContainer: ', err)
+    //   })
+     
+  }
   return (
     <div id='graphContainer'>
 
@@ -128,28 +86,32 @@ function LineGraphContainer(props) {
           <option value="kafka_server_broker_topic_metrics_replicationbytesinpersec_rate" >Kafka Broker Topic Metrics replication bytes In Per Sec</option>
           <option value="kafka_server_replica_manager_underreplicatedpartitions" >Kafka Replica Manager Under Replicated Partitions</option>
           <option value="kafka_server_replica_manager_failedisrupdatespersec" >Kafka Replica Manager Failed Updates Per Second</option>
-          {/* prometheus connection health */}
-          {/* <option value="scrape_duration_seconds" >{`Scrape Duration (seconds)`}</option>
-          <option value="scrape_samples_scraped" >{`Samples Scraped`}</option> */}
-          {/* pie chart metrics */}
-          {/* <option value="kafka_coordinator_group_metadata_manager_numgroups" >Kafka Number of Metadata Groups</option>
-          <option value="kafka_coordinator_group_metadata_manager_numgroupsdead" >Kafka Number of Dead Metadata Groups</option>
-          <option value="kafka_coordinator_group_metadata_manager_numgroupsempty" >Kafka Number of Empty Metadata Groups</option> */}
         </select>
 
-        {/* timeframe */}
-        <select id='timeframe' onChange={(e) => handleDynamicMetrics(e)}>
-          <option value="">Timeframe</option>
-          <option value="1m">1m</option>
-          <option value="5m">5m</option>
-          <option value="15m">15m</option>
-          <option value="30m">30m</option>
-          <option value="1h">1h</option>
-          <option value="2h">2h</option>
-          <option value="4h">4h</option>
-          <option value="6h">6h</option>
-          <option value="12h">12h</option>
-        </select>
+        {/* need form and submit */}
+        <Flatpickr
+          id = 'startTime'
+          data-enable-time
+          options = {
+            {
+              minuteIncrement: 1
+            }
+          }
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+        />
+        <Flatpickr
+          id = 'endTime'
+          data-enable-time
+          options = {
+            {
+              minuteIncrement: 1
+            }
+          }
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+        />
+          <button id='submitTime' onClick={(e) => handleHistorical(e)}>Submit</button>
 
         <button id="deleteGraph" onClick={(e) => handleChartDelete(e)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
@@ -159,11 +121,10 @@ function LineGraphContainer(props) {
         </button>
       </div>
 
-
       <LineChart chartData={chartData} />
 
     </div>
   )
 }
 
-export default LineGraphContainer;
+export default HistoricalGraphContainer;
