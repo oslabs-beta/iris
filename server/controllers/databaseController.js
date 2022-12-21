dfd6c91b7b9e2fbf7030057bb953c3dbfd97b288
Brennan <blee3395@gmail.com>
2022-12-20 21:29:29 -0500

    refactor: code cleanup

--- server/controllers/databaseController.js
+++ server/controllers/databaseController.js
@@ -1,5 +1,8 @@
 const db = require('./pg.ts'); 
 const fetch = require('node-fetch');
+
+const BASE_PATH = require('../../config/default')
+
 //------------------------------------------------------------------------------------------------------------//
 const keys = {
   // metric: identifier
@@ -21,8 +24,6 @@ const keys = {
 const dbController = {}
 dbController.getHistoricalData = (req, res, next) => {
   const { metric, startTime, endTime } = req.body
-
-  console.log('getHistoricalData times: ', startTime, endTime)
 
   const body = `SELECT * 
     FROM iris_database AS ib
@@ -59,7 +60,7 @@ dbController.getHistoricalData = (req, res, next) => {
 }
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_bytesinpersec_rate = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_broker_topic_metrics_bytesinpersec_rate[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_broker_topic_metrics_bytesinpersec_rate[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -107,7 +108,7 @@ dbController.add_bytesinpersec_rate = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_failedpartitionscount_value = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_replica_fetcher_manager_failedpartitionscount_value[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_replica_fetcher_manager_failedpartitionscount_value[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -151,7 +152,7 @@ dbController.add_failedpartitionscount_value = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_maxlag_value = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_replica_fetcher_manager_maxlag_value[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_replica_fetcher_manager_maxlag_value[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -195,7 +196,7 @@ dbController.add_maxlag_value = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_offlinereplicacount = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_replica_manager_offlinereplicacount[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_replica_manager_offlinereplicacount[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -239,7 +240,7 @@ dbController.add_offlinereplicacount = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_bytesoutpersec_rate = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_broker_topic_metrics_bytesoutpersec_rate[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_broker_topic_metrics_bytesoutpersec_rate[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -285,7 +286,7 @@ dbController.add_bytesoutpersec_rate = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_messagesinpersec_rate = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_broker_topic_metrics_messagesinpersec_rate[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_broker_topic_metrics_messagesinpersec_rate[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -331,7 +332,7 @@ dbController.add_messagesinpersec_rate = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_replicationbytesinpersec_rate = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_broker_topic_metrics_replicationbytesinpersec_rate[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_broker_topic_metrics_replicationbytesinpersec_rate[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -375,7 +376,7 @@ dbController.add_replicationbytesinpersec_rate = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_underreplicatedpartitions = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_replica_manager_underreplicatedpartitions[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_replica_manager_underreplicatedpartitions[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -419,7 +420,7 @@ dbController.add_underreplicatedpartitions = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_failedisrupdatespersec = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_replica_manager_failedisrupdatespersec[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_replica_manager_failedisrupdatespersec[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -463,7 +464,7 @@ dbController.add_failedisrupdatespersec = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_scrapedurationseconds = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=scrape_duration_seconds[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=scrape_duration_seconds[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -506,7 +507,7 @@ dbController.add_scrapedurationseconds = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_scrape_samples_scraped = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=scrape_samples_scraped[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=scrape_samples_scraped[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
@@ -549,7 +550,7 @@ dbController.add_scrape_samples_scraped = (lastTimeStamp) => {
 };
 //------------------------------------------------------------------------------------------------------------//
 dbController.add_requesthandleraverageidlepercent = (lastTimeStamp) => {
-  return fetch(`http://localhost:9090/api/v1/query?query=kafka_server_request_handler_avg_idle_percent[5m]`)
+  return fetch(BASE_PATH + `/api/v1/query?query=kafka_server_request_handler_avg_idle_percent[5m]`)
     .then(res => res.json())
     .then(data => {
       let body = `
