37e68747dd1016d292b78138f7f65c0fbd01ed5c
Brennan <blee3395@gmail.com>
2023-01-01 15:56:25 -0500

    chore: removed unused react dependencies

--- client/home/containers/HomeContainer.jsx
+++ client/home/containers/HomeContainer.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import RightPane from './RightPane.jsx';
 import LeftPane from './LeftPane.jsx';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#455a64',
+      darker: '#053e85',
+    },
+  },
+});
+
 function HomeContainer({ loading }) {
 
   return (
-    (loading) ? <Box sx={{ 
-        width: '90%', 
-        height: '100%',
-        p: 4,
-        pt:25,
-        m: 0
-        }}>
-        <LinearProgress />
-      </Box> : (
+    (loading) ? <ThemeProvider theme={theme}>
+        <Box sx={{ 
+          width: '90%', 
+          height: '100%',
+          p: 4,
+          pt:25,
+          m: 0, 
+          }}>
+          <LinearProgress color='primary' />
+        </Box>
+      </ThemeProvider> : (
       <div id='homePage'>
         <LeftPane />
         <RightPane />
