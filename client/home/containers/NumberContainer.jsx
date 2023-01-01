f8086ed57dac86efd282344b6d97d45ff5a00a05
Brennan <blee3395@gmail.com>
2022-12-31 20:25:02 -0500

    fix: update NumberContainer to show loaded data on render

--- client/home/containers/NumberContainer.jsx
+++ client/home/containers/NumberContainer.jsx
@@ -7,6 +7,37 @@ function NumberContainer(props) {
   const [partitionCount, setPartitionCount] = useState(0);
 
   useEffect(() => {
+    fetch('http://localhost:9090/api/v1/query?query=kafka_controller_globaltopiccount')
+      .then(res => res.json())  
+      .then(data => {
+        
+        setTopicCount(Number(data.data.result[0].value[1]))
+      })
+      .catch(err => {
+        console.log('Error thrown in GET request in numberContainer: ', err)
+      })
+    fetch('http://localhost:9090/api/v1/query?query=kafka_controller_globalpartitioncount')
+      .then(res => res.json())  
+      .then(data => {
+        setPartitionCount(Number(data.data.result[0].value[1]))
+      })
+      .catch(err => {
+        console.log('Error thrown in GET request in numberContainer globalPartitionCount: ', err)
+      })
+    fetch('http://localhost:9090/api/v1/query?query=kafka_server_broker_topic_metrics_bytesinpersec_rate')
+      .then(res => res.json())  
+      .then(data => {
+        const arr = []
+        for (let i = 0; i < data.data.result.length; i++) {
+          if (data.data.result[i].metric.topic){
+            arr.push(<li>{data.data.result[i].metric.topic}</li>)
+          }
+        }
+        setTopicArr(arr)
+      })
+      .catch(err => {
+        console.log('Error thrown in GET request in numberContainer topicMetrics: ', err)
+      })
     setInterval(() => {
       fetch('http://localhost:9090/api/v1/query?query=kafka_controller_globaltopiccount')
         .then(res => res.json())  
