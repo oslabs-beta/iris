6b5bad6c6a5a6e420ea4f297be733b18bb1a4f98
Brennan <blee3395@gmail.com>
2022-12-31 20:24:14 -0500

    feat: add LinearProgress MUI for loading state

--- client/home/containers/HomeContainer.jsx
+++ client/home/containers/HomeContainer.jsx
@@ -1,14 +1,27 @@
-import React, { Component, useState, useEffect } from 'react';
+import React from 'react';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
 import RightPane from './RightPane.jsx';
 import LeftPane from './LeftPane.jsx';
 
+function HomeContainer({ loading }) {
 
-function HomeContainer(props) {
   return (
-    <div id='homePage'>
-      <LeftPane />
-      <RightPane />
-    </div>
+    (loading) ? <Box sx={{ 
+        width: '90%', 
+        height: '100%',
+        p: 4,
+        pt:25,
+        m: 0
+        }}>
+        <LinearProgress />
+      </Box> : (
+      <div id='homePage'>
+        <LeftPane />
+        <RightPane />
+      </div>
+    )
   )
 };
 
