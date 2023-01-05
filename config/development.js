c6fdd5c3d768a77af75c77bd301c1a2d91ca8224
Brennan <blee3395@gmail.com>
2023-01-05 17:27:35 -0500

    fix: change to username reference for sequelize

--- config/development.js
+++ config/development.js
@@ -1,12 +1,14 @@
-const { BASE_PATH } = require('./default') 
+const { BASE_PATH } = require('./default');
 
 module.exports = {
-  BASE_PATH: BASE_PATH,
+  BASE_PATH,
   DB_TABLE: 'dev_table',
   DATABASE: {
     host: 'localhost',
-    user: 'dev_user',
+    username: 'dev_user',
     password: 'dev_pw',
     port: 8532,
-  }
-}
+    dialect: 'postgres',
+    logging: false, // Enable db sql query logging
+  },
+};
